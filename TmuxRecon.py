30c8ee353687b990f2e6e336bb8d12f3180c5c4b
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2021-04-25 14:11:07 -0400

    Add files via upload

---
+++ TmuxRecon.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import pwd
+import shlex
+import pprint
+import argparse
+import subprocess
+from netaddr import *
+
+parser  = argparse.ArgumentParser()
+parser.add_argument("IP", help="IP address of the target", type=str)
+args    = parser.parse_args()
+
+try:
+    IP      = IPAddress(args.IP)
+    LastOct = args.IP.split('.')[-1]
+except:
+    print("That does not seem to be a valid IP. Exiting...")
+    exit()
+
+cwd = os.getcwd()
+trd = subprocess.getoutput("readlink /usr/bin/TmuxRecon")
+trd = trd[:-12]
+
+hassession  = subprocess.run(shlex.split("tmux has-session -t TR_{}".format(LastOct)), stderr=subprocess.DEVNULL)
+if hassession.returncode == 0:
+    print("A session for {} already exists. Exiting".format(IP))
+    exit()
+else:
+    pass
+
+subprocess.run(shlex.split("tmux new-session -s TR_{} -n Main -c {} -d".format(LastOct, trd)))
+subprocess.run(shlex.split("tmux send-keys -t TR_{}:0.0 '{}app.py {} {} {} {}' Enter".format(LastOct, trd, IP, LastOct, cwd, trd,)))
+subprocess.run(shlex.split("tmux ls"))
