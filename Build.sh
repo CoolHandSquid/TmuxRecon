08a264220fcc618f8b33aa9e12c3087f16870bb4
CoolHandSquid <42553774+CoolHandSquid@users.noreply.github.com>
2021-04-25 14:12:03 -0400

    Add files via upload

---
+++ Build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#Verify in TmuxRecon directory
+if [[ "$(pwd | awk -F"/" '{print$NF}')" != "TmuxRecon" ]]; then
+	echo "You must be in the TmuxRecon directory to run this script (case sensitive)."
+	exit 1
+fi
+
+cat <<'END'
+TmuxRecon is installing...
+END
+
+if [[ ! -d 'dirsearch' ]]; then git clone https://github.com/maurosoria/dirsearch.git; fi
+chmod -R 755 dirsearch/
+
+rm /usr/bin/TmuxRecon 2> /dev/null
+ln -s "$PWD/TmuxRecon.py" "/usr/bin/TmuxRecon"
+
+apt update
+apt-get install gobuster seclists dconf-cli g++ pip libreoffice smtp-user-enum leafpad -y
+wait
+python3 -m pip install pandasql
+
+if [ -f "/usr/share/wordlists/rockyou.txt.gz" ] || [ ! -f "/usr/share/wordlists/rockyou.txt" ]
+then
+	gzip -d /usr/share/wordlists/rockyou.txt.gz
+fi
+
+wordlists=("/usr/share/wordlists/rockyou.txt" "/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt" "/usr/share/dirbuster/wordlists/directory-list-2.3-small.txt" "/usr/share/seclists/Usernames/Names/names.txt")
+for wordlist in "${wordlists[@]}"
+do
+    if [[ ! -f "$wordlist" ]]
+    then
+        echo "#"
+        echo "#"
+        echo "#"
+        echo "You are going to want to install $wordlist"
+        echo "#"
+        echo "#"
+        echo "#"
+    fi
+done
+chown 0 -R ./*
+chmod +x -R *
